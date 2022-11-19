374bfb91ca0214c58d861c4d747583f1a69a8f8e
Ahmed_Nazeer <ahmedgelany45@gmail.com>
2022-11-19 12:59:53 +0200

    create getAllHerosEndpoint

---
+++ Net7.WebAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Net7Demo.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAllHeros()
+        {
+            var superHerolsLst = new List<SuperHero> { 
+                new SuperHero 
+                { 
+                    Id = 1, 
+                    Name = "Spider man", 
+                    FirstName = "Peter", 
+                    LastName = "Parker", 
+                    Place = "New york" 
+                } 
+            };
+            return Ok(superHerolsLst);
+        }
+
+    }
+}
