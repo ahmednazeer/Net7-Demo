646e6c82e24e01715d719274430c883a097d1e3a
Ahmed_Nazeer <ahmedgelany45@gmail.com>
2022-11-19 13:10:07 +0200

    create getHerro endpoint to get cetain hero

--- Net7.WebAPI/Controllers/SuperHeroController.cs
+++ Net7.WebAPI/Controllers/SuperHeroController.cs
@@ -8,19 +8,28 @@ namespace WebAPI.Controllers
     [ApiController]
     public class SuperHeroController : ControllerBase
     {
+        private static List<SuperHero> superHerolsLst = new List<SuperHero> {
+            new SuperHero
+            {
+                Id = 2,
+                Name = "Spider man",
+                FirstName = "Peter",
+                LastName = "Parker",
+                Place = "New york"
+            }
+        };
         [HttpGet]
         public async Task<ActionResult<List<SuperHero>>> GetAllHeros()
         {
-            var superHerolsLst = new List<SuperHero> { 
-                new SuperHero 
-                { 
-                    Id = 1, 
-                    Name = "Spider man", 
-                    FirstName = "Peter", 
-                    LastName = "Parker", 
-                    Place = "New york" 
-                } 
-            };
+            return Ok(superHerolsLst);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SuperHero>> GetHero(int id)
+        {
+            var hero= superHerolsLst.Find(x => x.Id == id);
+            if(hero is null)
+                return NotFound("sorry , we can not find this here");
             return Ok(superHerolsLst);
         }
 
